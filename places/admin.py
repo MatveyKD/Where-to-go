b4598ec7de3e4ac055a3a82f885d101fee82cac6
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-09-08 19:31:19 +0300

    Replace json data with db

--- places/admin.py
+++ places/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Tag, Comment
+from .models import Place, Image
 
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'author', 'text')
-    raw_id_fields = ('post', 'author',)
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    raw_id_fields = ('author', 'likes',)
-
-admin.site.register(Tag)
+admin.site.register(Place)
+admin.site.register(Image)
