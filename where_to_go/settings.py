f911f10f763310d15b2076ad96c02bf9fe74619e
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-09-13 19:14:29 +0300

    Add text editor

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'places.apps.PlacesConfig',
+    'adminsortable2',
+    'tinymce'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
