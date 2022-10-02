ba1bc5427b802a0cbf5525ca851428822a82c452
Matvey256 <matvey.kiselev.10@bk.ru>
2022-10-02 14:11:51 +0300

    Fix comment

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don"t run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
